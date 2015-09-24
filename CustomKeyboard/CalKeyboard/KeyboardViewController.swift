0889ff66ad2eb4beedb5bf8e4ed45497a9d5ded2
Jonathan Bai <gamwang@berkeley.edu>
2015-09-23 23:57:58 -0700

    Finished hw2

--- CustomKeyboard/CalKeyboard/KeyboardViewController.swift
+++ CustomKeyboard/CalKeyboard/KeyboardViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class KeyboardViewController: UIInputViewController {
+    @IBOutlet weak var returnBUtton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var inputButton: UIButton!
 
     @IBOutlet var nextKeyboardButton: UIButton!
     
@@ -34,6 +37,19 @@ class KeyboardViewController: UIInputViewController {
         // The app is about to change the document's contents. Perform any preparation here.
     }
 
+    @IBAction func returnDown(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UIKeyInput
+        proxy.insertText("\n")
+    }
+    @IBAction func deleteDown(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UIKeyInput
+        proxy.insertText( "Deleting...")
+        proxy.deleteBackward()
+    }
+    @IBAction func inputDown(sender: AnyObject) {
+        let proxy = self.textDocumentProxy as UIKeyInput
+        proxy.insertText("writes some text on click yo!")
+    }
     override func textDidChange(textInput: UITextInput?) {
         // The app has just changed the document's contents, the document context has been updated.
     }
@@ -44,7 +60,7 @@ class KeyboardViewController: UIInputViewController {
         keyboardView.frame = view.frame
         view.addSubview(keyboardView)
         view.backgroundColor = keyboardView.backgroundColor
-        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template
+        nextKeyboardButton.addTarget(self, action: "advanceToNextInputMode", forControlEvents: .TouchUpInside) // advanceToNextInputMode is already defined in template\
     }
 
 
